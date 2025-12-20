0d4d901e806cb0c6424ef0a8dce9018a52b2cf7b
Marco <hsn103@gmail.com>
2025-12-21 00:52:49 +0900

    feat: Add Vue DevTools plugin to Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vueDevTools(), vue(), tailwindcss()],
   resolve: {
     alias: {
       "@": "/src",
