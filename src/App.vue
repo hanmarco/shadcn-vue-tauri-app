8ace3e2074dcbb4bd35293dc03cae841779903a6
Marco <hsn103@gmail.com>
2025-12-24 11:44:22 +0900

    fix: minor change on toaster ui

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ const activeTab = ref("config");
 </script>
 
 <template>
-  <Toaster position="bottom-right" close-button expand />
+  <Toaster position="bottom-right" close-button rich-colors />
   <MainLayout v-model:activeTab="activeTab">
     <div class="h-full flex flex-col overflow-hidden">
       <!-- Configuration View -->
